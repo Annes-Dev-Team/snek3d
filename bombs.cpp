a48d24a183855a7fd1ea41da2922a6ca57457f55
annes <anneswidow@gmail.com>
2026-05-16 16:29:50 +0300

    update raylib and make web support

---
+++ bombs.cpp
@@ -0,0 +1,12 @@
+#include "raylib/raylib.h"
+
+int main() {
+    int winw;
+    int winh;
+    InitWindow(800, 600, "Bombs");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+    
+    }
+    CloseWindow();
+}
