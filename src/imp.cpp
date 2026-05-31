b86822de451701d67f32466d53b7da980358972d
annes <anneswidow@gmail.com>
2026-05-31 17:09:24 -0500

    physac

---
+++ src/imp.cpp
@@ -0,0 +1,10 @@
+// implement raygui, rlights and physac 
+
+#define RAYGUI_IMPLEMENTATION
+#include <raylib/raygui.h>
+
+#define RLIGHTS_IMPLEMENTATION
+#include <raylib/rlights.h>
+
+#define PHYSAC_IMPLEMENTATION
+#include <raylib/physac.h>
